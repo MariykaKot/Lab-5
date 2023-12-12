66a5a47ad726484c0ebf91bcb97d4f8a0ab204da
MariykaKot <mskoturanova@gmail.com>
2023-12-12 14:11:49 +0300

    Laba_5.py

--- laba5.py
+++ laba5.py
@@ -1,4 +1,6 @@
 #Котуранова Мария Сергеевна 408879
+
+#1 Задание
 import requests
 import json
 
@@ -16,3 +18,38 @@ pressure=round(weather_data["main"]["pressure"])
 print('Сейчас в городе', city, str(temperature), 'градусов')
 print('Влажность в городе',  str(humidity), '%')
 print('Давление в городе',  str(pressure), 'hPa')
+
+#2 Задание
+#сайт: https://rickandmortyapi.com/
+
+import requests
+import json
+
+while True:
+    fact = input('1 - characters, 2 - locations, 3 - episodes, 0 - exit: ')
+    match fact:
+        case '1':
+            idi = input('Enter the id: ')
+            response = requests.get(f'https://rickandmortyapi.com/api/character/{idi}')
+            data = dict(response.json())
+            ind = ['id','name', 'status','gender','url','image', "episode"]
+            for x in range (len(ind)):
+                print(ind[x],':', data[ind[x]])
+        case '2':
+            idi = input('Enter the id: ')
+            response = requests.get(f'https://rickandmortyapi.com/api/location/{idi}')
+            data = dict(response.json())
+            ind = ['id', 'name', 'type', 'dimension', 'url']
+            for x in range (len(ind)):
+                print(ind[x],':', data[ind[x]])
+        case '3':
+            idi = input('Enter the id: ')
+            response = requests.get(f'https://rickandmortyapi.com/api/episode/{idi}')
+            data = dict(response.json())
+            ind = ['id', 'name', 'air_date', 'episode', 'url']
+            for x in range(len(ind)):
+                print(ind[x], ':', data[ind[x]])
+        case '0':
+            break
+        case _:
+            print("Wrong type")
